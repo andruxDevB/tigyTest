2df9dea29b0059b90ebe080448798263fd435dec
Christian Segovia <csegovia@grupocentrico.com>
2021-02-01 23:56:42 -0500

    tailwind banner

---
+++ components/TgTlBanner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-white shadow rounded-lg flex items-center">
+    <div class="flex-1 flex-grow">
+      <div class="p-6">
+        <h2 class="text-gray-600">
+          <strong>Solicita ayuda</strong> o
+          <strong>envía un reconocimiento</strong> a tus amigos
+        </h2>
+      </div>
+    </div>
+    <div class="flex-1 flex-grow-0">
+      <tg-tl-banner-action
+        icon="HandIcon"
+        label="Solicitar ayuda"
+      ></tg-tl-banner-action>
+    </div>
+    <div class="flex-1 flex-grow-0">
+      <tg-tl-banner-action
+        icon="GiftIcon"
+        label="Reconocimiento"
+      ></tg-tl-banner-action>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TgTlBanner',
+}
+</script>
---
+++ components/TgTlBannerAction.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+    class="group block w-full h-full focus:outline-none p-6 border-l border-gray-200"
+  >
+    <div class="flex flex-row justify-center items-center h-full">
+      <component :is="icon" :class="[baseIconClass]"></component>
+    </div>
+    <p :class="[baseTextClass]">{{ label }}</p>
+  </button>
+</template>
+<script>
+import HandIcon from '@nuxt-hero-icons/outline/src/components/HandIcon'
+import GiftIcon from '@nuxt-hero-icons/outline/src/components/GiftIcon'
+
+export default {
+  name: 'TgTlBannerAction',
+  components: {
+    HandIcon,
+    GiftIcon,
+  },
+  props: {
+    icon: {
+      type: String,
+      default: 'HandIcon',
+      required: true,
+    },
+    label: {
+      type: String,
+      default: null,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      baseIconClass: 'text-gray-600 w-8 h-8 group-hover:text-purple-500',
+      baseTextClass:
+        'text-gray-600 group-hover:text-purple-500 subpixel-antialiased px-2 py-1 tracking-wide leading-tight text-sm',
+    }
+  },
+}
+</script>
