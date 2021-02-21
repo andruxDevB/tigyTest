16967801a298b705cac866b9d51e0d9573ba0c9c
Christian Segovia <csegovia@grupocentrico.com>
2021-02-21 09:56:39 -0500

    fix tgline layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-    <tg-tl-banner></tg-tl-banner>
+  <div class="p-6">
+    <div class="container">
+      <div class="mx-auto max-w-xl">
+        <tg-tl-banner></tg-tl-banner>
 
-    <tg-post-skeleton v-if="$fetchState.pending" class="my-5" />
-    <transition
-      enter-active-class="ease-in-out duration-500"
-      leave-active-class="ease-in-out duration-500"
-      enter-class="opacity-0"
-      enter-to-class="opacity-100"
-      leave-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <tg-tl-events v-if="!$fetchState.pending" :events="events"></tg-tl-events>
-    </transition>
+        <tg-post-skeleton v-if="$fetchState.pending" class="my-5" />
+        <transition
+          enter-active-class="ease-in-out duration-500"
+          leave-active-class="ease-in-out duration-500"
+          enter-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <tg-tl-events
+            v-if="!$fetchState.pending"
+            :events="events"
+          ></tg-tl-events>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
