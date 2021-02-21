bc0570daecf00f409ea8b08be245634e45f0c9b5
Christian Segovia <csegovia@grupocentrico.com>
2021-02-21 11:05:29 -0500

    loading messenger

---
+++ components/TgMessengerChatSkeleton.vue
@@ -0,0 +1,42 @@
+<template>
+  <section
+    class="min-w-0 flex-1 h-full flex flex-col overflow-hidden xl:order-last"
+  >
+    <div class="animate-pulse flex-shrink-0 border-b">
+      <!-- Toolbar-->
+      <div class="pt-4 px-4s m:px-6 lg:px-8">
+        <div class="flex space-x-4 mb-4">
+          <div class="rounded-full bg-gray-200 h-11 w-11"></div>
+          <div class="flex-1 space-y-2 py-1">
+            <div class="h-4 bg-gray-300 rounded w-2/4"></div>
+            <div class="h-3 bg-gray-300 rounded w-1/4"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Message header -->
+    </div>
+    <div
+      class="animate-pulse min-h-0 space-y-4 flex-1 overflow-y-auto p-4 px-4sm:px-6 lg:px-8"
+    >
+      <div class="flex-1 space-y-4">
+        <div class="rounded bg-gray-200 h-8 w-2/4"></div>
+        <div class="rounded bg-gray-200 h-8 w-1/4"></div>
+        <div class="rounded bg-gray-200 h-8 w-3/4"></div>
+        <div class="rounded bg-gray-200 h-8 w-1/6"></div>
+      </div>
+
+      <div class="flex space-y-4 justify-end">
+        <div class="rounded bg-gray-200 h-8 w-1/4"></div>
+      </div>
+      <div class="flex space-y-4 justify-end">
+        <div class="rounded bg-gray-200 h-8 w-3/4"></div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'TgMessengerChatSkeleton',
+}
+</script>
--- pages/messenger/_id.vue
+++ pages/messenger/_id.vue
@@ -1,5 +1,10 @@
 <template>
-  <tg-messenger-chat v-if="!$fetchState.pending"></tg-messenger-chat>
+  <div class="flex w-full">
+    <tg-messenger-chat v-if="!$fetchState.pending"></tg-messenger-chat>
+    <tg-messenger-chat-skeleton
+      v-if="$fetchState.pending"
+    ></tg-messenger-chat-skeleton>
+  </div>
 </template>
 <script>
 export default {
