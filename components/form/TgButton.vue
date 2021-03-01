6df77e01b110c72f029321b579a6c76a2b15edcb
Christian Segovia <csegovia@grupocentrico.com>
2021-03-01 14:52:30 -0500

    add loding state button

--- components/form/TgButton.vue
+++ components/form/TgButton.vue
@@ -1,5 +1,26 @@
 <template>
-  <button :class="[baseClass, buttonClass, customClass]">
+  <button :class="[baseClass, buttonClass, customClass]" :disabled="loading">
+    <svg
+      v-if="loading"
+      class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
     <slot>Label</slot>
   </button>
 </template>
@@ -15,11 +36,21 @@ export default {
       type: String,
       default: '',
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {
       baseClass:
         'group flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium focus:ring-offset-2',
+      loadingClass: {
+        primary:
+          'text-white bg-purple-400 focus:outline-none focus:ring-2 focus:ring-purple-700 shadow-sm pointer-events-none',
+        flat:
+          'text-gray-500 hover:bg-gray-50 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500',
+      },
       typeClass: {
         primary:
           'text-white hover:bg-purple-700 bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500 shadow-sm',
@@ -31,7 +62,9 @@ export default {
   },
   computed: {
     buttonClass() {
-      return this.typeClass[this.type]
+      return this.loading
+        ? this.loadingClass[this.type]
+        : this.typeClass[this.type]
     },
   },
 }
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -61,12 +61,9 @@
       </div>
 
       <div>
-        <button
-          type="submit"
-          class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
-        >
+        <tg-button custom-class="w-full" :loading="loading">
           Inicia sesión
-        </button>
+        </tg-button>
       </div>
     </ValidationObserver>
   </div>
