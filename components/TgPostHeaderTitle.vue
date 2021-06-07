aede31babbac644e69d1fb2f702b7e9465deae17
Christian Segovia <danger.cris@gmail.com>
2021-06-06 19:10:33 -0500

    link to profile in post component

--- components/TgPostHeaderTitle.vue
+++ components/TgPostHeaderTitle.vue
@@ -3,19 +3,25 @@
     <template v-if="type === 'GRATITUDE_SENT'">
       Enviaste un Agradecimiento a
       <outline-chevron-right-icon class="text-gray-300 mx-1 h-4 w-4" />
-      <strong class="text-gray-600">{{ user.full_name }}</strong>
+      <nuxt-link :to="`/users/${user.id}`" class="text-gray-600 font-bold">{{
+        user.full_name
+      }}</nuxt-link>
     </template>
     <template v-else-if="type === 'GRATITUDE_RECIVED'" class="flex">
       Recibiste un Agradecimiento de
       <outline-chevron-right-icon class="text-gray-300 mx-1 h-4 w-4" />
-      <strong class="text-gray-600">{{ user.full_name }}</strong>
+      <nuxt-link :to="`/users/${user.id}`" class="font-bold text-gray-600">{{
+        user.full_name
+      }}</nuxt-link>
     </template>
     <template v-else class="flex">
       <template v-if="currentUser.user_id === user.id">
         Solicitaste una ayuda
       </template>
       <template v-else>
-        <strong class="text-gray-600">{{ user.full_name }}</strong>
+        <nuxt-link :to="`/users/${user.id}`" class="font-bold text-gray-600">{{
+          user.full_name
+        }}</nuxt-link>
         <outline-chevron-right-icon class="text-gray-300 mx-1 h-4 w-4" />
         solicitó una ayuda
       </template>
