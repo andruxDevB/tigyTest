aaca892981d098883488134c8dd390ce73ecb5bb
Jorge Luis Veliz <jorge.veliz@novaseguroslatam.com>
2023-02-14 15:52:49 -0500

    cambios en el menu

--- store/nav/state.js
+++ store/nav/state.js
@@ -1,19 +1,20 @@
 export default () => ({
   mobile: false,
-  items: defaultMenus,
+  items: [...defaultMenus, gifMenu],
   itemsAdmin: [...defaultMenus, ...navAdmin],
 })
+
+const gifMenu = {
+  label: 'Premios',
+  link: '/gifs',
+  icon: 'GiftIcon',
+}
 
 const defaultMenus = [
   {
     label: 'Tigy Line',
     link: '/',
     icon: 'HomeIcon',
-  },
-  {
-    label: 'Premios',
-    link: '/gifs',
-    icon: 'GiftIcon',
   },
   {
     label: 'Mi Perfil',
